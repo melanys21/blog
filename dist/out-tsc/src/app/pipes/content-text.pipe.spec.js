754dcd2285dcbcbd2ac1220021ffd29f6fa93167
melanys21 <melo01211998@gmail.com>
2020-05-20 17:14:00 -0500

    Fix: add git ignore

--- dist/out-tsc/src/app/pipes/content-text.pipe.spec.js
+++
@@ -1,8 +0,0 @@
-import { ContentTextPipe } from './content-text.pipe';
-describe('ContentTextPipe', () => {
-    it('create an instance', () => {
-        const pipe = new ContentTextPipe();
-        expect(pipe).toBeTruthy();
-    });
-});
-//# sourceMappingURL=content-text.pipe.spec.js.map
